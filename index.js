2800982ce54086a6a38672e65a73bd6de64c9d2d
Steven Lundy <steven.lundy@gmail.com>
2018-03-03 16:13:07 -0800

    Refactor to get verb/noun homophone functions

--- index.js
+++ index.js
@@ -17,6 +17,7 @@ function getHomophones(word) {
             md: 'fp', // include metadata for frequency (f) and part of speech (p)
             max: 50 // limit to 50 responses
         },
+        headers: {'Cache-Control': 'max-age=86400'},
         json: true
     });
 }
@@ -29,47 +30,46 @@ function getSyllables(words) {
     }, []);
 }
 
-async function findNounVerbPairs(word1, word2) {
-    const getFrequency = function(item) {
-        let freqTag = item.tags.find(tag => tag.startsWith("f:"));
-        return freqTag ? +freqTag.split(":")[1] : 0;
-    };
-    const isCloseEnough = item => item.numSyllables == 1 && item.score >= 97 && getFrequency(item) > 1;
+const getFrequency = function(item) {
+    let freqTag = item.tags.find(tag => tag.startsWith("f:"));
+    return freqTag ? +freqTag.split(":")[1] : 0;
+};
+
+const isCloseEnough = item => item.numSyllables == 1 && item.score >= 97 && getFrequency(item) > 1;
+
+async function getNounHomophones(word) {
     const isNoun = item => item.tags.includes('n');
+    let homophones = await getHomophones(word);
+    homophones = homophones.filter(isCloseEnough);
+    let nouns = new Set(homophones.filter(isNoun).map(i => i.word));
+    if (await wordPOS.isNoun(word)) {
+        nouns.add(word);
+    }
+    return nouns;
+}
+
+async function getVerbHomophones(word) {
     const isVerb = item => item.tags.includes('v');
+    let homophones = await getHomophones(word);
+    homophones = homophones.filter(isCloseEnough);
+    let verbs = new Set(homophones.filter(isVerb).map(i => i.word));
+    if (await wordPOS.isVerb(word)) {
+        verbs.add(word);
+    }
+    return verbs;
+}
 
-    let word1Homophones = await getHomophones(word1);
-    word1Homophones = word1Homophones.filter(isCloseEnough);
-    word1Nouns = new Set(word1Homophones.filter(isNoun).map(i => i.word));
-    if (await wordPOS.isNoun(word1)) {
-        word1Nouns.add(word1);
-    }
-    word1Verbs = new Set(word1Homophones.filter(isVerb).map(i => i.word));
-    if (await wordPOS.isVerb(word1)) {
-        word1Verbs.add(word1);
-    }
-    let word2Homophones = await getHomophones(word2);
-    word2Homophones = word2Homophones.filter(isCloseEnough);
-    word2Nouns = new Set(word2Homophones.filter(isNoun).map(i => i.word));
-    if (await wordPOS.isNoun(word2)) {
-        word2Nouns.add(word2);
-    }
-    word2Verbs = new Set(word2Homophones.filter(isVerb).map(i => i.word));
-    if (await wordPOS.isVerb(word2)) {
-        word2Verbs.add(word2);
-    }
-
+async function findNounVerbPairs(word1, word2) {
     let nounVerbPairs = [];
-    word1Nouns.forEach(function(noun) {
-        word2Verbs.forEach(function(verb) {
-            nounVerbPairs.push([noun, verb]);
+    await Promise.all([[word1, word2], [word2, word1]].map(async function(words) {
+        let nouns = await getNounHomophones(words[0]);
+        let verbs = await getVerbHomophones(words[1]);
+        nouns.forEach(function(noun) {
+            verbs.forEach(function(verb) {
+                nounVerbPairs.push([noun, verb]);
+            });
         });
-    });
-    word2Nouns.forEach(function(noun) {
-        word1Verbs.forEach(function(verb) {
-            nounVerbPairs.push([noun, verb]);
-        });
-    });
+    }));
     return nounVerbPairs;
 }
 
