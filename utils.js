48afe6a0a2b6173194e038d2088e7ef084f790d3
Steven Lundy <steven.lundy@gmail.com>
2018-03-03 17:21:57 -0800

    Swap out hyphenation library for syllable splitting library

--- utils.js
+++ utils.js
@@ -1,12 +1,12 @@
-const Hypher = require('hypher'),
-    english = require('hyphenation.en-us'),
-    h = new Hypher(english);
 const Inflectors = require("en-inflectors").Inflectors;
+const nlp = require('nlp_compromise'),
+    nlpSyllables = require('nlp-syllables');
+    nlp.plugin(nlpSyllables);
 
 module.exports.makeSingular = word => new Inflectors(word).toSingular();
 
 module.exports.getSyllables = function(words) {
     return words.split(" ").reduce(function(syllables, word) {
-        return syllables.concat(h.hyphenate(word));
+        return syllables.concat(nlp.term(word).syllables());
     }, []);
 };
